98121c3e89997402d3e8b0d79a77fb7e5811cb50
Adeelnarejo <adeel.nariai96@gmail.com>
2025-06-27 12:15:43 +0500

    Initial commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ url = ("https://newsapi.org/v2/everything?q=tesla&from=2025-05-27&sortBy=publish
 request = requests.get(url)
 content = request.json()
 
+
 for article in content["articles"]:
     print(article["title"])
     print(article["description"])
