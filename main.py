b53d5244a450380ed206fdaf6827b4118764cd6c
Adeelnarejo <adeel.nariai96@gmail.com>
2025-06-27 12:14:57 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+
+# api key
+api_key = "c25984fdf8c340b3b2c773ecb9dcffff"
+
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2025-05-27&sortBy=publishedAt&apiKey"
+       "=c25984fdf8c340b3b2c773ecb9dcffff")
+
+request = requests.get(url)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
