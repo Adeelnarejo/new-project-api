74d16139b6a4821a24037b1e3ea2018067cb9631
adeel narejo <adeel.nariai96@gmail.com>
2025-06-30 11:33:54 +0500

    Update nasa.py

--- nasa.py
+++ nasa.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 
-api_key = "38Xi1DQdWMEUYlOy6tQ7cYdEnTQ9Digdw49fZ9eM"
+api_key = "####"
 url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 
 response = requests.get(url).json()
